7730bbb81c709b6bad827f3034b98ccc9a259d95
Khalid2214 <khalidshabir68@gmail.com>
2026-04-09 15:58:58 +0530

    test webhook api

--- risk_detection.py
+++ risk_detection.py
@@ -13,7 +13,7 @@ GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 if not GITHUB_TOKEN:
     print("Warning: GITHUB_TOKEN not found. You may hit rate limits.")
 
-
+print("Hey, can you add this this?")
 # ===============================
 # FETCH PR DIFF
 # ===============================
